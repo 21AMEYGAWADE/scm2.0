89fd1509a6f700d08bdb5639b3759fe43acbf1fd
21AMEYGAWADE <gawadeamey21@gmail.com>
2026-03-30 18:58:56 +0530

    Initial commit

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,41 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class PageController {
+
+    // Home page 
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("home controller called");
+
+        //Sending data to the html page using model (LINE:11)
+
+        model.addAttribute("title", "Home Page");
+        model.addAttribute("message", "Welcome to the Home Page!");
+        model.addAttribute("description", "This is the home page of our application.");
+
+        return "home";
+    }
+
+    // About page
+    @RequestMapping("/about")
+    public String aboutPage(Model model){
+
+        model.addAttribute("isLogin", false);
+
+        System.out.println("about controller called");
+        return "about";
+    }
+
+    // Services page
+    @RequestMapping("/services")
+    public String servicesPage(){
+        System.out.println("services controller called");
+        return "services";
+    }
+
+}
