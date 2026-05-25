79a6907cf3d64bf2b082a556140044d46b89ab1c
21AMEYGAWADE <gawadeamey21@gmail.com>
2026-05-25 14:11:18 +0530

    7.00_hrs done

---
+++ src/main/java/com/scm/controllers/UserController.java
@@ -0,0 +1,45 @@
+
+
+//THIS PAGE MANAGES REQUESTES RELATED TO USER ONLY //
+
+
+
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    // user dashboard
+    @RequestMapping(value="/dashboard")
+    public String userDashboard() {
+        return "user/dashboard";
+    }
+    
+    // user profile
+    @RequestMapping(value="/profile")
+    public String userProfile() {
+        return "user/profile";
+    }
+
+
+
+
+    // user add contact
+
+    // view contact
+
+    // edit contact
+
+    // delete contact
+
+    // search contact
+
+
+
+}
